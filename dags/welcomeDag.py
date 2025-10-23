feb5a104613c58288c504b96ecd0a640c3f8c13a
Marcus <s09203547@gamil.com>
2025-10-23 14:26:56 +0800

    revise deploys dags yaml and write a demo dag

--- dags/welcomeDag.py
+++ dags/welcomeDag.py
@@ -1,2 +1,35 @@
-from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.decorators import dag
+from datetime import timedelta
 
+default_args = {
+    'owner': 'marcus',
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def Hello(name: str):
+    print(f"Hello {name}")
+
+@dag(dag_id="test_dag", default_args=default_args, catchup=False, tags=['test_dag'])
+def welcome_dag():
+
+    start_time = BashOperator(
+        task_id='start_time', 
+        bash_command='date > /opt/airflow/test.txt'
+    )
+    greet_task = PythonOperator(
+        task_id='greet_task',
+        python_callable=Hello,
+        op_args=['Airflow User'],
+    )
+
+    bash_task = BashOperator(
+        task_id='bash_task',
+        bash_command='echo "This is a bash task!"',
+    )
+
+    start_time >> greet_task >> bash_task
+
+welcome_dag()
